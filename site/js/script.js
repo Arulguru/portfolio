29d71c2271b5055f7edba714d4db261fcbaa2f83
arulguru <arulguru@kali.guru>
2021-03-25 00:29:02 +0530

    certificates

--- site/js/script.js
+++ site/js/script.js
@@ -1,3 +1,31 @@
-$("#box").click(function(){
-	$("#box").animate({"color":"red"},2000);
-});
+$("#BlockchainBasics").click(function(){
+	$("#certificateBox").html("<img src='Images/BlockchainBasics.jpg' style='width: 100%; border: 1px solid white; padding: 5px'></img>");
+})
+$("#smct").click(function(){
+	$("#certificateBox").html("<img src='Images/smartContracts.jpg' style='width: 100%; border: 1px solid white; padding: 5px'></img>");
+})
+$("#dapp").click(function(){
+	$("#certificateBox").html("<img src='Images/DApps.jpg' style='width: 100%; border: 1px solid white; padding: 5px'></img>");
+})
+$("#js").click(function(){
+	$("#certificateBox").html("<img src='Images/introToJS.jpg' style='width: 100%; border: 1px solid white; padding: 5px'></img>");
+})
+$("#fullstack").click(function(){
+	$("#certificateBox").html("<img src='Images/html,css,js.jpg' style='width: 100%; border: 1px solid white; padding: 5px'></img>");
+})
+
+$("#py").click(function(){
+	$("#certificateBox").html("<img src='Images/SKILLRACK_PY.png' style='width: 100%; border: 1px solid white; padding: 5px'></img>");
+})
+$("#java").click(function(){
+	$("#certificateBox").html("<img src='Images/SKILLRACK_JAVA.png' style='width: 100%; border: 1px solid white; padding: 5px'></img>");
+})
+$("#sql").click(function(){
+	$("#certificateBox").html("<img src='Images/sql.jpg' style='width: 100%; border: 1px solid white; padding: 5px'></img>");
+})
+$("#c").click(function(){
+	$("#certificateBox").html("<img src='Images/SKILLRACK_C.png' style='width: 100%; border: 1px solid white; padding: 5px'></img>");
+})
+$("#dsa").click(function(){
+	$("#certificateBox").html("<img src='Images/ds.jpg' style='width: 100%; border: 1px solid white; padding: 5px'></img>");
+})
